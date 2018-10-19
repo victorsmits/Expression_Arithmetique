d48a79f66aa8a7f87d22aabb95913f2e7b1b8f6b
victorsmits <victor.smits@student.vinci.be>
2018-10-19 17:29:23 +0200

    update test
    non gérée "+ + ab − cd " et "ab + cd − +"

--- Expression_Arithmetique/NUnitTestClass.cs
+++ Expression_Arithmetique/NUnitTestClass.cs
@@ -10,16 +10,27 @@ namespace Expression_arithmétique
         {
             //infixe test
             Assert.AreEqual(3, MainClass.EvaluatePostfixExpr("1+2"));
+            Assert.AreEqual(2, MainClass.EvaluatePostfixExpr("1*2"));
+            Assert.AreEqual(-1, MainClass.EvaluatePostfixExpr("1-2"));
+            Assert.AreEqual(2, MainClass.EvaluatePostfixExpr("2/1"));
             //postfixe test
             Assert.AreEqual(3, MainClass.EvaluatePostfixExpr("12+"));
+            Assert.AreEqual(2, MainClass.EvaluatePostfixExpr("12*"));
+            Assert.AreEqual(-1, MainClass.EvaluatePostfixExpr("12-"));
+            Assert.AreEqual(2, MainClass.EvaluatePostfixExpr("21/"));
             //préfixe test
             Assert.AreEqual(3, MainClass.EvaluatePostfixExpr("+12"));
+            Assert.AreEqual(2, MainClass.EvaluatePostfixExpr("*12"));
+            Assert.AreEqual(-1, MainClass.EvaluatePostfixExpr("-12"));
+            Assert.AreEqual(2, MainClass.EvaluatePostfixExpr("/21"));
             //infixe groupe test
             Assert.AreEqual(6, MainClass.EvaluatePostfixExpr("(1+2)+3"));
             Assert.AreEqual(9, MainClass.EvaluatePostfixExpr("(1+2)*3"));
             Assert.AreEqual(0, MainClass.EvaluatePostfixExpr("(1+2)-3"));
             Assert.AreEqual(1, MainClass.EvaluatePostfixExpr("(1+2)/3"));
             Assert.AreEqual(8, MainClass.EvaluatePostfixExpr("(1+2)+(3+2)"));
+
+            //NON GERER
             //infixe groupe test
             //Assert.AreEqual(8, MainClass.EvaluatePostfixExpr("++12+32"));
             //postfixe groupe test
